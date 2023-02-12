930e49d369fa1f07e423499871e2a8908218577b
Jeff Jones <spjeff@spjeff.com>
2023-02-11 19:51:10 -0600

    Init

---
+++ SPJeff-Turbo-SQL-to-SharePoint-List-Sync.ps1
@@ -0,0 +1,200 @@
+<#
+.SYNOPSIS
+    Read source SQL table and synchronize into SharePoint Online (SPO) List destination.
+.DESCRIPTION
+    Turbo sync SQL table to SharePoint Online List.  Optimized for speed. Insert, Update, and Delete to match rows and columns.  Great for PowerPlatform, PowerApps, and PowerBI integration scenarios.  Simple PS1 to run from on-premise virtual machine Task Scheduler.  No need for Data Managment Gateway (DMG) or complex firewall rules.  Optimized with usage of primary key, index columns, [Compare-Object] cmdlet [System.Data.DataRow] type, hashtable, and PNP batch HTTP POST network traffic.  Includes transcript LOG for support.
+
+    * Script will delete rows from the SharePoint List that are not in the SQL table.  
+    * Script will add rows to the SharePoint List that are not in the SharePoint List.  
+    * Script will update rows in the SharePoint List that are different in the SQL table.
+    * Script will not update rows in the SharePoint List that are the same in the SQL table.
+.EXAMPLE
+    C:\> .\SPJeff-Turbo-SQL-to-SharePoint-List-Sync.ps1
+.NOTES
+    File Name:  SPJeff-Turbo-SQL-to-SharePoint-List-Sync.ps1
+    Version:    1.0
+    Author:     Jeff Jones - @spjeff
+    Modified:   2021-02-25
+.LINK
+    https://github.com/spjeff/Turbo-SQL-to-SharePoint-List-Sync
+#>
+
+# DotNet Assembly
+using namespace System.Data
+using namespace System.Collections.Generic
+
+function Write-Yellow($message) {
+    Write-Host $message -ForegroundColor "Yellow"
+}
+
+# PowerShell Modules
+# from https://stackoverflow.com/questions/28740320/how-do-i-check-if-a-powershell-module-is-installed
+Write-Yellow "Loading PowerShell modules..."
+@("SQLServer", "PNP.PowerShell") |% {
+    if (!(Get-Module -ListAvailable -Name $_)) {
+        Write-Yellow "Installing module: $_"
+        Install-Module -Name $_ -Force
+    } else {
+        Write-Yellow "Loading module: $_"
+        Import-Module $_ -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue" | Out-Null
+    }
+}
+
+# Helper functions
+# from https://www.powershellgallery.com/packages/ConvertFrom-DataRow/0.9.1/Content/ConvertFrom-DataRow.psm1
+function ConvertFrom-DataRow {
+    [CmdletBinding( DefaultParameterSetName = 'AsHashtable' )]
+    [OutputType( [System.Collections.Specialized.OrderedDictionary], ParameterSetName = 'AsHashtable' )]
+    [OutputType( [pscustomobject], ParameterSetName = 'AsObject' )]
+    param(
+        [Parameter( Mandatory, Position = 0, ValueFromPipeline = $true )]
+        [DataRow[]]
+        $InputObject,
+        [Parameter( Mandatory, ParameterSetName = 'AsObject' )]
+        [switch]
+        $AsObject,
+        $DbNullValue = $null
+    )
+    begin {
+        [List[string]]$Columns = @()
+    }
+    process {
+        foreach ( $DataRow in $InputObject ) {
+            if ( $Columns.Count -eq 0 ) {
+                $Columns.AddRange( [string[]]$DataRow.Table.Columns.ColumnName )
+            }
+            $ReturnObject = @{}
+            $Columns | ForEach-Object {
+                if ( $DataRow.$_ -is [System.DBNull] ) {
+                    $ReturnObject[$_] = $DbNullValue
+                }
+                else {
+                    $ReturnObject[$_] = $DataRow.$_
+                }
+            }
+            if ( $AsObject ) {
+                Write-Output ( [pscustomobject]$ReturnObject )
+            }
+            else {
+                Write-Output ( $ReturnObject )
+            }
+        }
+    }
+}
+
+# Main application
+function Main() {
+    # STEP 1 - Connect SQL
+    $sqlQuery = "SELECT * FROM [dbo].[Customer]"
+    $sqlServer = "spjeff-sql.database.windows.net"
+    $sqlDatabase = "northwind"
+    $sqlUser = ""
+    $sqlPass = ""
+    $sqlPrimaryKey = "CustomerId"
+    $sqlSource = Invoke-Sqlcmd -Query $sqlQuery -ServerInstance $sqlServer -Database $sqlDatabase -Username $sqluser -Password $sqlpass
+    $sqlSourceHash = $sqlSource | ConvertFrom-DataRow
+
+    # STEP 2 - Connect SPO
+    $spUrl = "https://spjeffdev.sharepoint.com/sites/Turbo-SQL-to-SharePoint-List-Sync"
+    $spClientId = ""
+    $spClientSecret = ""
+    $spListName = "Customer" 
+    $spMatchItem = $null
+
+    # Dynamic schema.  SPLIST always has [Id] and [Title] fields.  Append SQL columns to SPLIST fields
+    $spFields = "Id", $sqlPrimaryKey
+    $sqlSource[0].Table.Columns |? {$_.ColumnName -ne $sqlPrimaryKey} |% { $spFields += $_.ColumnName}
+
+    # Connect to SPO and get SPLIST items with dynamic schema
+    Connect-PnPOnline -Url $spUrl -ClientId $spClientId -ClientSecret $spClientSecret -WarningAction "Silentlycontinue"
+    $spDestination = Get-PnPListItem -List Customer -Fields $spFields -PageSize "4000"
+
+    # Measure changes to SPLIST
+    $added   = 0
+    $updated = 0
+    $deleted = 0
+
+    # Measure SPLIST rows before and after
+    $beforeCount = $spDestination.Count
+    $afterCount  = $sqlSource.Count
+
+    # STEP 3 - Delete excess SPLIST items on destination by comparing primary keys
+    foreach ($item in $spDestination) { 
+        # Primary Key
+        $pk = $item[$sqlPrimaryKey]
+
+        # Loop comparison
+        if ($sqlSource.$sqlPrimaryKey -notcontains $pk) {
+            # Delete row
+            Remove-PnPListItem -List $spListName -Identity $item.Id -Force
+            $deleted = $deleted + 1
+            Write-Yellow "Deleted: $pk"
+        }
+    }
+
+    # STEP 4 - Add or update SPLIST items on destination by comparing primary keys
+    $spBatch = New-PnPBatch
+    foreach ($row in $sqlSourceHash) {
+        # Flatten SQL row to hashtable
+        $hash = ($row | Select-Object ($spFields | Select-Object -Skip 1))
+
+        # Primary key search, if row exists
+        $pk = $row[$sqlPrimaryKey]
+        $spMatchItem = $spDestination | ? { $_[$sqlPrimaryKey] -eq $pk }
+
+        # First matched SPLIST row only
+        if ($spMatchItem -is [System.Array]) {
+            $spMatchItem = $spMatchItem[0]
+        }
+
+        # Format objects consistently for [Compare-Object] support
+        if ($spMatchItem) {
+            $hashsp = ([Hashtable]$spMatchItem.FieldValues) | Select-Object ($spFields | Select-Object -Skip 1)
+            $hashsp.CustomerId = [int]$hashsp.CustomerId
+        }
+
+        # If row does not exist, add it
+        if (!$spMatchItem) {
+            # Insert row
+            $item = Add-PnPListItem -List $spListName -Values $row -Batch $spBatch
+            $added++
+            Write-Yellow "Added: $sqlPrimaryKey = $pk"
+        }
+        else {
+            # If row does exist, update it
+            $needUpdate = Compare-Object $hash $hashsp
+            if ($needUpdate) {
+                # Update row
+                Write-Yellow "Updated: $pk"
+                Set-PnPListItem -List $spListName -Identity $spMatchItem.Id -Values $hash -Batch $spBatch
+                $updated = $updated + 1
+                $needUpdate = $false
+            }
+        }
+    }
+
+    # Invoke HTTP POST network request
+    Invoke-PnPBatch $spBatch
+
+    # Display results with SQL and SPLIST row counts
+    Write-Yellow "Source Rows        = $($beforeCount)"
+    Write-Yellow "Destination Rows = $($afterCount)"
+    Write-Yellow "---"
+    Write-Yellow "Added      = $added"
+    Write-Yellow "Updated    = $updated"
+    Write-Yellow "Deleted    = $deleted"
+}
+
+# Open LOG with script name
+$prefix = $MyInvocation.MyCommand.Name
+$host.UI.RawUI.WindowTitle = $prefix
+$stamp = Get-Date -UFormat "%Y-%m-%d-%H-%M-%S"
+Start-Transcript "$PSScriptRoot\log\$prefix-$stamp.log"
+$start = Get-Date
+Main
+
+# Close LOG and display time elapsed
+$end = Get-Date
+$totaltime = $end - $start
+Write-Yellow "`nTime Elapsed: $($totaltime.tostring("hh\:mm\:ss"))"
+Stop-Transcript
